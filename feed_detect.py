e975c59f827831b1dcfb326a4ed8653c58156b9f
gyorvaripeter <gyorvaripeter>
2023-02-16 02:34:51 +0100

    modify logic

--- feed_detect.py
+++ feed_detect.py
@@ -1,1 +1,8 @@
+from ultralytics import YOLO
+import cv2
 
+
+model = YOLO("ultralytics/yolo/best(2).pt")
+# accepts all formats - image/dir/Path/URL/video/PIL/ndarray. 0 for webcam
+#results = model.predict(source="0")
+results = model.predict(source="/home/gyorvaripeter/Letöltések/csgo_mirage_killfeed_frames/", show=False, imgsz=1280, save_txt=True, save=True) # Display preds. Accepts all YOLO predict arguments
